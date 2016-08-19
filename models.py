bd63be23e7e0a69ae52033102a996b815761b336
Christian Barra <barrachri@gmail.com>
2016-08-19 08:32:14 +0200

    small corrections

--- models.py
+++ models.py
@@ -1,18 +1,17 @@
+# Stdlib imports
 import datetime
 from functools import wraps
+# 3rd party lib imports
 from peewee import *
 
 db = SqliteDatabase(None)
 
-# model definitions -- the standard "pattern" is to define a base model class
-# that specifies which database to use.  then, any subclasses will automatically
-# use the correct storage.
 class BaseModel(Model):
     class Meta:
         database = db
 
-# the user model specifies its fields (or columns) declaratively, like django
 class User(BaseModel):
+    """Users model"""
     email = CharField(unique=True)
     first_name = CharField()
     last_name = CharField()
